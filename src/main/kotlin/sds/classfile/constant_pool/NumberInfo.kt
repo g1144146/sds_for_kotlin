9812dede6b77a87ca5207337ffc3b1c2468aa48a
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-05-19 15:11:00 +0900

    delete unnecessary classes.

--- src/main/kotlin/sds/classfile/constant_pool/NumberInfo.kt
+++ src/main/kotlin/sds/classfile/constant_pool/NumberInfo.kt
@@ -1,16 +1,13 @@
 package sds.classfile.constant_pool
 
-sealed class NumberInfo(private val number: Number, type: Int): Constant(type) {
-    override fun toString(): String = super.toString() + "\t" + when(tag) {
-        Type.INT    -> number.toInt()
-        Type.FIELD  -> number.toFloat()
-        Type.LONG   -> number.toLong()
-        Type.DOUBLE -> number.toDouble()
+class NumberInfo(private val number: Number, type: Int): Constant(type) {
+    fun getValue(): Number = when(number) {
+        is Int    -> number.toInt()
+        is Float  -> number.toFloat()
+        is Long   -> number.toLong()
+        is Double -> number.toDouble()
         else -> throw IllegalStateException("unknown number type.")
     }
-}
 
-class IntInfo(val value: Int):       NumberInfo(value, Type.INT)
-class FloatInfo(val value: Float):   NumberInfo(value, Type.FLOAT)
-class LongInfo(val value: Long):     NumberInfo(value, Type.LONG)
-class DoubleInfo(val value: Double): NumberInfo(value, Type.DOUBLE)
+    override fun toString(): String = super.toString() + "\t" + getValue()
+}
