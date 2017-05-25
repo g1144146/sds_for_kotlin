c8e99df60058e2c9141dc90f4c5d0186128e8952
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-05-25 17:15:00 +0900

    change to interface.

--- src/main/kotlin/sds/classfile/constant_pool/MemberInfo.kt
+++ src/main/kotlin/sds/classfile/constant_pool/MemberInfo.kt
@@ -1,6 +1,6 @@
 package sds.classfile.constant_pool
 
-class MemberInfo(val type: Int, val _class: Int, val nameAndType: Int): Constant() {
+class MemberInfo(val type: Int, val _class: Int, val nameAndType: Int): Constant {
     override fun toString(): String = when(type) {
         Type.FIELD  -> "Field_ref"
         Type.METHOD -> "Method_ref"
